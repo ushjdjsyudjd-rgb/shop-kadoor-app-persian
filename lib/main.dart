81a96c34602c36a347b3202eccbbc2e2edc8a7d0
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2025-12-31 19:31:55 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:shop_app_flutter/constants.dart';
-import 'package:shop_app_flutter/screens/home/home_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'constants.dart'; // برای استفاده از رنگ‌هایی که تنظیم کردیم
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarIconBrightness: Brightness.dark,
-    systemNavigationBarColor: Colors.white,
-    systemNavigationBarIconBrightness: Brightness.dark,
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primaryColor: kPrimaryColor),
+    home: KadoorHomePage(),
   ));
-  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class KadoorHomePage extends StatefulWidget {
+  @override
+  _KadoorHomePageState createState() => _KadoorHomePageState();
+}
 
-  // This widget is the root of your application.
+class _KadoorHomePageState extends State<KadoorHomePage> {
+  List products = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchProducts();
+  }
+
+  // اتصال مستقیم به گوگل شیت شما
+  Future<void> fetchProducts() async {
+    try {
+      final response = await http.get(Uri.parse('https://sheetdb.io/api/v1/czatwlt4x2234'));
+      if (response.statusCode == 200) {
+        setState(() {
+          products = json.decode(response.body);
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: Theme.of(context)
-            .textTheme
-            .apply(bodyColor: kTextColor, fontFamily: 'IranSans'),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("کادور پخش شمال"),
+        backgroundColor: kPrimaryColor,
+        centerTitle: true,
       ),
-      home: const Directionality(
-        textDirection: TextDirection.rtl,
-        child: Homescreen(),
-      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator(color: kPrimaryColor))
+          : GridView.builder(
+              padding: EdgeInsets.all(kDefaultPadding),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.75,
+                crossAxisSpacing: kDefaultPadding,
+                mainAxisSpacing: kDefaultPadding,
+              ),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.2), blurRadius: 5)],
+                  ),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                          child: Image.network(products[index]['image'], fit: BoxFit.cover),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(products[index]['title'], style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Text("${products[index]['price']} تومان", style: TextStyle(color: kPrimaryColor)),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                );
+              },
+            ),
     );
   }
 }
-
-//pixle 6 Android 11 my Emulator 
